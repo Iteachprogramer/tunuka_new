5b7058871973ab9fe37c26879b358057ecddd1c7
Shukurullo <shukurullo0321@gmail.com>
2021-10-14 14:49:10 +0500

    date picker options improved

--- soft/widget/kartik/DatePicker.php
+++ soft/widget/kartik/DatePicker.php
@@ -1,8 +1,11 @@
 <?php
-
+/*
+ * @author Shukurullo Odilov
+ * @link telegram: https://t.me/yii2_dasturchi
+ * @date 24.07.2021, 10:06
+ */
 
 namespace soft\widget\kartik;
-
 
 use soft\helpers\ArrayHelper;
 use yii\base\InvalidArgumentException;
@@ -11,27 +14,22 @@ use yii\helpers\Html;
 class DatePicker extends \kartik\widgets\DatePicker
 {
 
-    public $language = 'ru';
+    public $dateFormat = 'd.m.Y';
+    public $placeholder = 'KK-MM-YYYY';
 
+    public $autoclose = true;
     public $todayHighlight = true;
 
-    public function run()
-    {
-        parent::run();
-    }
+    public $language = 'ru';
 
     public function init()
     {
+        $this->normalizeValues();
+        $this->normalizeAttributes();
+        $this->registerCss();
+
         parent::init();
 
-        $this->normalizeValues();
-        $this->normalizeOptions();
-        $css = <<<CSS
-        .datepicker-dropdown{
-            z-index: 9999!important;
-        }
-CSS;
-        $this->view->registerCss($css, [], 'datepicker-dropdown-css');
     }
 
     private function normalizeValues()
@@ -44,23 +42,55 @@ CSS;
                     throw new InvalidArgumentException('Attribute name must contain word characters only.');
                 }
                 $attribute = $matches[2];
-                $this->value = date('d.m.Y', $value);
-                $this->model->$attribute = date('d.m.Y', $value);
+                $this->value = date($this->dateFormat, $value);
+                $this->model->$attribute = date($this->dateFormat, $value);
             }
         } else {
+
             $value = $this->value;
             if (!empty($value) && is_integer($value)) {
-                $this->value = date('d.m.Y', $value);
+                $this->value = date($this->dateFormat, $value);
             }
+
         }
+
     }
 
-    private function normalizeOptions()
+    private function registerCss()
     {
-        if ($this->todayHighlight) {
-            ArrayHelper::setValue($this->pluginOptions, 'todayHighlight', true);
+        $css = <<<CSS
+        .datepicker-dropdown{
+            z-index: 9999!important;
         }
+CSS;
+
+        $this->view->registerCss($css, [], 'datepicker-dropdown-css');
     }
 
+    /**
+     * @return array
+     */
+    private function defaultPluginOptions()
+    {
+        return [
+            'autoclose' => $this->autoclose,
+            'todayHighlight' => $this->todayHighlight,
+        ];
+    }
 
+    /**
+     * @return array
+     */
+    private function defaultOptions()
+    {
+        return [
+            'placeholder' => $this->placeholder,
+        ];
+    }
+
+    private function normalizeAttributes()
+    {
+        $this->pluginOptions = ArrayHelper::merge($this->pluginOptions, $this->defaultPluginOptions());
+        $this->options = ArrayHelper::merge($this->options, $this->defaultOptions());
+    }
 }

7f3e95037dd5c77fe9d3ab22fd03550e3429ecb0
Dostonbek <otajonov1117@mail.ru>
2022-06-20 11:32:27 +0500

    error_new

--- backend/views/outcome-group/_columns.php
+++ backend/views/outcome-group/_columns.php
@@ -41,7 +41,7 @@ return [
                 'timePicker' => true,
                 'timePickerIncrement' => 30,
                 'locale' => [
-                    'format' => 'd.m.Y H:i:s'
+                    'format' => 'd.m.Y'
                 ]
             ]
         ],

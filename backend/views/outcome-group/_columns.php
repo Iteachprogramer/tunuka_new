7e12f6c881f4273e081db065256c5b4d30caf892
Dostonbek <otajonov1117@mail.ru>
2022-06-20 11:08:13 +0500

    error_new

--- backend/views/outcome-group/_columns.php
+++ backend/views/outcome-group/_columns.php
@@ -29,8 +29,7 @@ return [
         'attribute' => 'date',
         'width' => '160px',
         'value' => function(OutcomeGroup $model){
-            return Yii::$app->formatter->asDatetime($model->date, 'dd.MM.yyyy');
-
+            return Yii::$app->formatter->asDate($model->date, 'dd.MM.yyyy');
         },
         'filterType' => GridView::FILTER_DATE_RANGE,
         'filterWidgetOptions' => [

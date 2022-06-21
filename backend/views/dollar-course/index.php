a72f67fa88174d207a5b2eb0ad853eadd10509e0
Dostonbek <otajonov1117@mail.ru>
2022-06-21 17:13:35 +0500

    course

--- backend/views/dollar-course/index.php
+++ backend/views/dollar-course/index.php
@@ -1,9 +1,10 @@
 <?php
+
+use soft\grid\GridView;
+use soft\widget\ajaxcrud\CrudAsset;
 use yii\helpers\Url;
 use yii\helpers\Html;
 use yii\bootstrap4\Modal;
-use kartik\grid\GridView;
-use johnitvn\ajaxcrud\CrudAsset;
 use johnitvn\ajaxcrud\BulkButtonWidget;
 
 /* @var $this yii\web\View */
@@ -14,37 +15,24 @@ $this->title = 'Dollar Kursi';
 $this->params['breadcrumbs'][] = $this->title;
 
 CrudAsset::register($this);
-
 ?>
-<div class="dollar-course-index">
-    <div id="ajaxCrudDatatable">
-        <?=GridView::widget([
-            'id'=>'crud-datatable',
-            'dataProvider' => $dataProvider,
-            'filterModel' => $searchModel,
-            'pjax'=>true,
-            'columns' => require(__DIR__.'/_columns.php'),
-            'toolbar'=> [
-                ['content'=>
-                    Html::a('<i class="fas fa-redo"></i>', [''],
-                    ['data-pjax'=>1, 'class'=>'btn btn-secondary', 'title'=>'Reset Grid']).
-                    '{toggleData}',
+    <div class="dollar-course-index">
+        <div id="ajaxCrudDatatable">
+            <?= GridView::widget([
+                'id' => 'crud-datatable',
+                'dataProvider' => $dataProvider,
+                'filterModel' => $searchModel,
+                'pjax' => true,
+                'columns' => require(__DIR__ . '/_columns.php'),
+                'toolbarButtons' => [
+                    'create' => false,
                 ],
-            ],
-            'striped' => true,
-            'condensed' => true,
-            'responsive' => true,
-            'panel' => [
-                'type' => 'primary',
-                'heading' => false,
-                'footer' => false,
-            ]
-        ])?>
+            ]) ?>
+        </div>
     </div>
-</div>
 <?php Modal::begin([
-    "id"=>"ajaxCrudModal",
+    "id" => "ajaxCrudModal",
     "title" => '<h4 class="modal-title">Modal title</h4>',
-    "footer"=>"",// always need it for jquery plugin
-])?>
+    "footer" => "",// always need it for jquery plugin
+]) ?>
 <?php Modal::end(); ?>

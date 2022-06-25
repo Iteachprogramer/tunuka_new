b96db6205659c93438862b148da995fbd169b5a5
Dostonbek <otajonov1117@mail.ru>
2022-06-25 10:57:02 +0500

    client-report

--- backend/views/client/index.php
+++ backend/views/client/index.php
@@ -1,11 +1,13 @@
 <?php
 
+use common\models\Client;
 use kartik\daterange\DateRangePicker;
 use soft\grid\GridView;
 use soft\widget\ajaxcrud\CrudAsset;
 use yii\bootstrap4\Modal;
 use yii\helpers\Html;
 use yii\helpers\Url;
+use yii\web\View;
 
 /* @var $this yii\web\View */
 /* @var $searchModel common\models\search\ClientSearch */
@@ -17,33 +19,8 @@ $this->params['breadcrumbs'][] = $this->title;
 CrudAsset::register($this);
 
 ?>
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-6" style="width: 100%">
-            <form action="<?= Url::to(['/client/index']) ?>" class="form-inline export-form">
-                <?php
-                /** @var TYPE_NAME $model */
-                echo DateRangePicker::widget([
-                    'name' => 'range',
-                    'attribute' => 'date_range',
-                    'presetDropdown' => true,
-                    'includeMonthsFilter' => true,
-                    'startAttribute' => 'datetime_min',
-                    'endAttribute' => 'datetime_max',
-                    'pluginOptions' => [
-                        'timePickerIncrement' => 30,
-                        'locale' => [
-                            'format' => 'Y-m-d H:i:s'
-                        ]
-                    ]
-                ]);
-                ?>
-                <?= Html::submitButton('<i class="fas fa-search"></i> Hisobot olish', ['class' => 'btn btn-primary text-white export', 'style' => 'margin-left:5px']) ?>
-            </form>
-        </div>
-    </div>
-    <br>
-</div><!-- /.container-fluid -->
+<a id="downloadLink" onclick="exportF(this)" class="btn btn-primary fa fa-file-excel-o"
+   style="margin-bottom: 15px;padding: 10px"> Hisobot olish</a>
 
 <div class="client-index">
     <div id="ajaxCrudDatatable">
@@ -51,6 +28,7 @@ CrudAsset::register($this);
             'id' => 'crud-datatable',
             'pagerDropDown' => true,
             'exportButton' => false,
+            'pjax' => false,
             'toolbarButtons' => [
                 'create' => [
                     'pjax' => false,
@@ -72,24 +50,87 @@ CrudAsset::register($this);
     "title" => '<h4 class="modal-title">Modal title</h4>',
     "footer" => "",// always need it for jquery plugin
 ]) ?>
-<div id="export-div" style="display: none">
-
+<div class="card">
+    <div class="card-body">
+        <div class="table-responsive">
+            <table border="1" cellspacing="0" cellpadding="3" id="myTable"
+                   style="text-align: center; align-items: center;display: none;width: 100%!important;"
+                   class="table table-bordered table-striped">
+                <tr>
+                    <td colspan="6" style="vertical-align: middle; text-align: center"> Xaqdorlar va qarizdorlar</td>
+                </tr>
+                <tr>
+                    <td colspan="2" style="vertical-align: middle; text-align: center">Sana:</td>
+                    <td colspan="4" style="vertical-align: middle; text-align: center"><?=Yii::$app->formatter->asDate(time(),'php:d.m.Y')?></td>
+                </tr>
+                <tr>
+                    <td style="vertical-align: middle; text-align: center">№</td>
+                    <td style="vertical-align: middle; text-align: center">Mijoz nomi</td>
+                    <td style="vertical-align: middle; text-align: center">Qarzi so'mda</td>
+                    <td style="vertical-align: middle; text-align: center">Qarzi dollarda</td>
+                    <td style="vertical-align: middle; text-align: center">Xaqqi so'mda</td>
+                    <td style="vertical-align: middle; text-align: center">Xaqqi dollarda</td>
+                </tr>
+                <?php
+                $clients = $dataProvider->getModels();
+                ?>
+                <?php foreach ($clients as $key => $client): ?>
+                    <tr>
+                        <td style="vertical-align: middle; text-align: center"><?= $key + 1 ?></td>
+                        <td style="vertical-align: middle; text-align: center"><?= $client->fulla_name ?></td>
+                        <td style="vertical-align: middle; text-align: center">
+                            <?php
+                            if ($client->finishAccountSum > 0) {
+                                echo $client->finishAccountSum;
+                            } else {
+                                echo '0';
+                            }
+                            ?>
+                        </td>
+                        <td style="vertical-align: middle; text-align: center">
+                            <?php
+                            if ($client->finishAccountSumDollar > 0) {
+                                echo $client->finishAccountSumDollar;
+                            } else {
+                                echo '0';
+                            }
+                            ?>
+                        </td>
+                        <td style="vertical-align: middle; text-align: center">
+                            <?php
+                            if ($client->finishAccountSum < 0) {
+                                echo $client->finishAccountSum;
+                            } else {
+                                echo '0';
+                            }
+                            ?>
+                        </td>
+                        <td style="vertical-align: middle; text-align: center">
+                            <?php
+                            if ($client->finishAccountSumDollar < 0) {
+                                echo $client->finishAccountSum;
+                            } else {
+                                echo '0';
+                            }
+                            ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+        </div>
+    </div>
 </div>
 <?php Modal::end(); ?>
 <?php
 $js = <<<JS
-$(document).on('click', '.export', function(e){
-    e.preventDefault();
-    $.ajax({
-            url: $('.export-form').attr('action'),
-            data: $('.export-form').serialize(),
-            success: function(result)
-            {
-                
-            }
-        })
-})
+        function exportF(elem) {
+        var table = document.getElementById("myTable");
+        var html = table.outerHTML;
+        var url = 'data:application/vnd.ms-excel,' + '\uFEFF' + encodeURIComponent(html); // Set your html table into url
+        elem.setAttribute("href", url);
+        elem.setAttribute("download", "Mijozlar.xls"); // Choose the file name
+        return false;
+    }
 JS;
-$this->registerJs($js);
+$this->registerJs($js, View::POS_HEAD);
 ?>
-

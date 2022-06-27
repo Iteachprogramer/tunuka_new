c90fb2fdbb80eca14187fdd94eac0b5376ec61bf
Dostonbek <otajonov1117@mail.ru>
2022-06-27 11:42:05 +0500

    statistics

--- common/models/Outcome.php
+++ common/models/Outcome.php
@@ -255,7 +255,7 @@ class Outcome extends \soft\db\ActiveRecord
             ->andWhere(['outcome.type_id' => ProductList::TYPE_RULON])
             ->andWhere(['<=', 'outcome.date', $today])
             ->andWhere(['>=', 'outcome.date', $lastMonth])
-            ->limit(7)
+            ->limit(10)
             ->with('productType')
             ->asArray()
             ->all();
